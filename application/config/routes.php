360a1cb6a5373f269844cc720f244de8c82546d5
Akhyar <akhyrul@gmail.com>
2013-01-09 14:36:14 +0700

    Clean up routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,58 +1,52 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/*
-| -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	http://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There area two reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router what URI segments to use if those provided
-| in the URL cannot be matched to a valid route.
-|
-*/
+
+
+/**
+ * RESERVED ROUTES
+ */
 
 $route['default_controller'] = "staffs";
 $route['404_override'] = '';
 
-// Auth
+
+
+/**
+ * AUTH
+ */
+
 $route['login']  = 'auth/login';
 $route['logout'] = 'auth/logout';
 $route['forget'] = 'auth/forget';
 
-// Backend
-$route['admin']         = 'admin/index';
-$route['admin/profile'] = 'admin/profile';
-$route['admin/article'] = 'admin/article';
-$route['admin/file']    = 'admin/file';
 
-// Frontend
+
+/**
+ * BACKEND
+ */
+
+$route['admin']                       = 'admin/home/index';
+
+$route['admin/profile']               = 'admin/profile/index';
+$route['admin/profile/edit']          = 'admin/profile/edit';
+$route['admin/profile/password']      = 'admin/profile/password';
+
+$route['admin/article']               = 'admin/article/index';
+$route['admin/article/create']        = 'admin/article/create';
+$route['admin/article/(:num)/edit']   = 'admin/article/edit/$1';
+$route['admin/article/(:num)/delete'] = 'admin/article/delete/$1';
+
+$route['admin/file']                  = 'admin/file/index';
+$route['admin/file/create']           = 'admin/file/create';
+$route['admin/file/(:num)/edit']      = 'admin/file/edit/$1';
+$route['admin/file/(:num)/upload']    = 'admin/file/upload/$1';
+$route['admin/file/(:num)/delete']    = 'admin/file/delete/$1';
+
+
+
+/**
+ * FRONTEND
+ */
+
 $route['(:any)/download']              = 'staffs/download/$1/1';
 $route['(:any)/download/(:num)']       = 'staffs/download/$1/$2';
 $route['(:any)/blog']                  = 'staffs/blog/$1/1';
@@ -60,7 +54,12 @@ $route['(:any)/blog/(:num)']           = 'staffs/blog/$1/$2';
 $route['(:any)/article/(:num)-(:any)'] = 'staffs/article/$1/$2/$3';
 $route['(:any)/profile']               = 'staffs/profile/$1';
 
-// Else
+
+
+/**
+ * ELSE
+ */
+
 $route['(:any)']                       = 'staffs/index/$1';
 
 
