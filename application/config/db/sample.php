37d33c7f91ea67735cc5ed768b105f13c565dbd5
Akhyar <akhyrul@gmail.com>
2013-01-09 15:51:43 +0700

    Add sample configuration file

---
+++ application/config/db/sample.php
@@ -0,0 +1,19 @@
+<?php 
+
+return array(
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => '',
+	'dbdriver' => 'mysql',
+	'dbprefix' => '',
+	'pconnect' => TRUE,
+	'db_debug' => TRUE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'autoinit' => TRUE,
+	'stricton' => FALSE,
+	);
